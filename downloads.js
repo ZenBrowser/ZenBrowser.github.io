9fba0a94247e251d42673dc5964287b3f4f13cb3
ZenBrowser <zenbrowser.business@gmail.com>
2025-04-05 12:21:24 -0400

    Scrapping some dumb stuff I don't need.

--- downloads.js
+++
@@ -1,7 +0,0 @@
-function downloadNewest() {
-    location.href = "https://github.com/PartehDev/ZenBrowser/releases/download/Alpha/ZenBrowser-Alpha-v0.0.1.zip" // to be updated later
-}
-
-function downloadAlpha() {
-    location.href = "https://github.com/PartehDev/ZenBrowser/releases/download/Alpha/ZenBrowser-Alpha-v0.0.1.zip" // to be updated later
-}
