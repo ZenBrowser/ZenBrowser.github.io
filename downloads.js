0d2f4bac2ed3d411098a2c6243f0aa8278694f7f
ZenBrowser <zenbrowser.business@gmail.com>
2025-04-05 12:05:09 -0400

    Add website files. [INITIAL COMMIT]

---
+++ downloads.js
@@ -0,0 +1,7 @@
+function downloadNewest() {
+    window.open("https://github.com/PartehDev/ZenBrowser/releases/download/Alpha/ZenBrowser-Alpha-v0.0.1.zip", "_blank");
+}
+
+function downloadAlpha() {
+    window.open("https://github.com/PartehDev/ZenBrowser/releases/download/Alpha/ZenBrowser-Alpha-v0.0.1.zip", "_blank");
+}
---
+++ index.js
@@ -0,0 +1,11 @@
+function goToDownloads() {
+    location.href = "https://partehdev.github.io/ZenBrowser/downloads/"
+}
+
+function goToContact() {
+    location.href = "https://partehdev.github.io/ZenBrowser/contact/"
+}
+
+function goToRepo() {
+    location.href = "https://github.com/PartehDev/ZenBrowser/"
+}
