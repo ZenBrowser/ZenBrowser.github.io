178097ada91e34efc08e5452fb6e06b800ca7d91
ZenBrowser <zenbrowser.business@gmail.com>
2025-04-05 12:19:43 -0400

    Update downloads.js

--- downloads.js
+++ downloads.js
@@ -1,7 +1,7 @@
 function downloadNewest() {
-    window.open("https://github.com/PartehDev/ZenBrowser/releases/download/Alpha/ZenBrowser-Alpha-v0.0.1.zip", "_blank");
+    location.href = "https://github.com/PartehDev/ZenBrowser/releases/download/Alpha/ZenBrowser-Alpha-v0.0.1.zip" // to be updated later
 }
 
 function downloadAlpha() {
-    window.open("https://github.com/PartehDev/ZenBrowser/releases/download/Alpha/ZenBrowser-Alpha-v0.0.1.zip", "_blank");
+    location.href = "https://github.com/PartehDev/ZenBrowser/releases/download/Alpha/ZenBrowser-Alpha-v0.0.1.zip" // to be updated later
 }
